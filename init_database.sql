9017be03860bac2c0a78338ee3d9c775708c76d3
briandataengineering <brian.dataengineering@gmail.com>
2026-04-13 12:12:23 +0300

    Create init_database.sql

---
+++ init_database.sql
@@ -0,0 +1,25 @@
+/*
+A code to create the warehouse, bronze, silver and gold schemas
+*/
+
+
+CREATE SCHEMA `project-7dde7e10-dbf5-4abe-b54.DataWarehouse`
+OPTIONS(
+  location="US"
+);
+
+CREATE SCHEMA `project-7dde7e10-dbf5-4abe-b54.bronze`
+OPTIONS(
+  location="US"
+);
+
+CREATE SCHEMA `project-7dde7e10-dbf5-4abe-b54.silver`
+OPTIONS(
+  location="US"
+);
+
+CREATE SCHEMA `project-7dde7e10-dbf5-4abe-b54.gold`
+OPTIONS(
+  location="US"
+);
+
