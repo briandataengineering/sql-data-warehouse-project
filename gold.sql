2c005c3549d8be703ca482633d1dd269bc4d5841
briandataengineering <brian.dataengineering@gmail.com>
2026-04-15 20:21:08 +0300

    Update gold.sql

--- gold.sql
+++ gold.sql
@@ -40,3 +40,52 @@ LEFT JOIN `project-7dde7e10-dbf5-4abe-b54.silver.erp_CUST_AZ12` ce
 ON ci.cst_key = ce.CID
 LEFT JOIN `project-7dde7e10-dbf5-4abe-b54.silver.erp_LOC_A101` cl
 ON ci.cst_key = cl.string_field_0
+
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  PRODUCT FINAL_TABLE
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+SELECT 
+      ROW_NUMBER() OVER(ORDER BY pc.prd_start_dt,pc.prd_key ) AS product_key,
+      pc.prd_id AS product_id,
+      pc.prd_key AS product_number,
+      pc.cat_id AS category_id,
+      pc.prd_nm AS product_name,
+      pc.prd_cost AS cost,
+      pc.prd_line AS product_line,
+      pc.prd_start_dt AS start_date,
+      pc.prd_end_dt AS end_date,
+      pe.CAT AS category,
+      pe.SUBCAT AS subcategory,
+      CASE WHEN MAINTENANCE IS TRUE THEN 'Yes'
+           ELSE 'No'
+           END AS maintenance
+FROM `project-7dde7e10-dbf5-4abe-b54.silver.crm_prd_info` pc
+LEFT JOIN `project-7dde7e10-dbf5-4abe-b54.silver.erp_PX_CAT_G1V2` pe
+ON pc.cat_id= pe.ID
+
+
+CREATE VIEW gold.dim_product AS 
+SELECT 
+      ROW_NUMBER() OVER(ORDER BY pc.prd_start_dt,pc.prd_key ) AS product_key,
+      pc.prd_id AS product_id,
+      pc.prd_key AS product_number,
+      pc.cat_id AS category_id,
+      pc.prd_nm AS product_name,
+      pc.prd_cost AS cost,
+      pc.prd_line AS product_line,
+      pc.prd_start_dt AS start_date,
+      pc.prd_end_dt AS end_date,
+      pe.CAT AS category,
+      pe.SUBCAT AS subcategory,
+      CASE WHEN MAINTENANCE IS TRUE THEN 'Yes'
+           ELSE 'No'
+           END AS maintenance
+FROM `project-7dde7e10-dbf5-4abe-b54.silver.crm_prd_info` pc
+LEFT JOIN `project-7dde7e10-dbf5-4abe-b54.silver.erp_PX_CAT_G1V2` pe
+ON pc.cat_id= pe.ID
+
+
+
+
